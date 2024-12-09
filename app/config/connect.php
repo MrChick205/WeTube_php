b1a1e89c313785694de347fbed17bca07e253725
MrChick205 <hvhiepbru@gmail.com>
2024-12-09 20:04:32 +0700

    fixxed first commit

---
+++ app/config/connect.php
@@ -0,0 +1,19 @@
+<?php
+	// session_start();
+	$host = "localhost";
+	$user = "root";
+	$password = "Hiep@1609";
+	$database = "wetube";
+
+	// Create connection
+	$conn = mysqli_connect($host,$user,$password,$database);
+	mysqli_set_charset($conn, 'UTF8');
+
+	// Check connection
+	if (!$conn) {
+    	die("Connection failed: " . mysqli_connect_error());
+	}else{
+        echo 'connect successful!';
+    }
+
+?>
