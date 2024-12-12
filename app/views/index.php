779d9a10d76397f620459abbdbe2097eb42c3c8d
MrChick205 <hvhiepbru@gmail.com>
2024-12-12 20:28:58 +0700

    feat/logout/done

--- app/views/index.php
+++ app/views/index.php
@@ -5,9 +5,22 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['user_email'])) {
     $userId = $_SESSION['user_id'];
     $userEmail = $_SESSION['user_email'];
 
-    echo "User ID thanh cong: " . htmlspecialchars($userId);
+    echo "User ID: " . htmlspecialchars($userId);
     echo "User Email: " . htmlspecialchars($userEmail);
 } else {
     echo "Người dùng chưa đăng nhập.";
 }
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <a href="auth/logout.php"><button>logout</button></a>
+</body>
+</html>
