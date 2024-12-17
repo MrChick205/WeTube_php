1581ea22c8604579a31e565da60d6c3d3fbbbbd2
hothitiep02 <htt301202@gmail.com>
2024-12-17 20:58:57 +0700

    displayDetail

---
+++ app/views/displayDetail.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Detail Display</title>
+</head>
+    <!-- Ionicons -->
+    <script src="https://unpkg.com/ionicons@5.5.2/dist/ionicons.js"></script>
+<style>
+.img_card {
+    margin-bottom: 10px;
+}
+</style>
+<body>
+    <div class="container mt-5">
+        <div class="row mb-4">
+            <div class="movie_card col-md-4">
+                <div class="img_card">
+                    <img src="https://phimimg.com/upload/vod/20241105-1/ff9b778b3f26b2b82af290a6b306c40f.jpg" class="img-fluid" alt="Movie Image">
+                </div>
+               <div class="btn_movie">
+                <a href="#" class="btn_dpl btn btn-danger"><ion-icon name="caret-forward-outline" style="font-size: 24px;"></ion-icon>Xem phim</a>
+               </div>
+            </div>
+            <div class="col-md-8">
+                <h1>LOVE</h1>
+                <p>describle</p>
+                <p>hdgfyebhdgfyuafyuagfua</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h2>Similar movies</h2>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <img src="https://phimimg.com/upload/vod/20241114-1/b145f04a857ff4f425308b1592da0cf0.jpg" class="card-img-top" alt="Movie 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Movie Title 1</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <img src="https://phimimg.com/upload/vod/20241031-1/17d28211ff9cfb483570b17a7ab00570.jpg" class="card-img-top" alt="Movie 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Movie Title 2</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <img src="https://phimimg.com/upload/vod/20241113-1/8aea15a945bd6c5018ac2a42c6be0a6c.jpg" class="card-img-top" alt="Movie 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Movie Title 3</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <img src="https://phimimg.com/upload/vod/20230920-1/7af5eee1d213d731ac8e01dacad7ac05.jpg" class="card-img-top" alt="Movie 4">
+                    <div class="card-body">
+                        <h5 class="card-title">Movie Title 4</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-1Z5bWn0Q3uHq0v3C5G6o74zQ6T1T4p1cl8k6D2jJ4hU5e9G5s2W0T5QwEBRz3A5R" crossorigin="anonymous"></script>
+</body>
+</html>
