86473cb4cbc5515105f747864c984ba351f07f44
Hoài Tiên05 <bohonuochthihoaitien@gmail.com>
2024-12-27 11:34:29 +1345

    fix/delete

--- app/views/admin/movies.php
+++ app/views/admin/movies.php
@@ -1,6 +1,21 @@
-<?php 
-    require_once('C:\xamppp\htdocs\Wetube\WeTube_php\app\controllers\movie.php');
+<?php
+require_once('C:\xamppp\htdocs\Wetube\WeTube_php\app\controllers\movie.php');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['add'])) {
+        $moviectrll->addMovie($_POST['title'], $_POST['description'], $_POST['movie_url'], $_POST['type_id'], $_POST['poster']);
+    } elseif (isset($_POST['update'])) {
+        $moviectrll->updateMovie($_POST['movie_id'], $_POST['title'], $_POST['description'], $_POST['movie_url'], $_POST['type_id'], $_POST['poster']);
+    } elseif (isset($_POST['delete'])) {
+        $moviectrll->deleteMovie($_POST['movie_id']);
+    }
+}
+
+$movies = $moviectrll->getAllMovies();
+
+
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,8 +23,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<style>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
     body {
     background-color: black;
 }
@@ -110,12 +126,22 @@
 .text-center {
     color: white;
 }
+.btn-form button{
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 8px;
+    width: 80px;
+    margin-left: 30px;
+    align-items: center;
+}
 </style>
+</head>
+
 <body>
     <div class="main-content">
         <div id="movieManagement" class="hidden">
             <h1 class="text-center">Movie management</h1>
-            <button id="createMovieBtn" class="add-btn btn my-3">Add</button>
+            <button id="createMovieBtn" class="add-btn btn my-3" data-bs-toggle="modal" data-bs-target="#createMovieModal">Add</button>
             <table id="movieTable" class="table table-striped">
     <thead class="table-dark">
         <tr>
@@ -142,8 +168,11 @@
                                 <td>{$movieitem['description']}</td>
                                 <td>{$movieitem['created_at']}</td>
                                 <td>
-                                    <button type='button' class='btn btn-warning'>Update</button>
-                                    <button type='button' class='btn btn-danger'>Delete</button>
+                                        <form action='movie.php' method='POST' style='display:inline;' class='btn-form'>
+                                            <button type='button' class='btn btn-warning'>Update</button>
+                                            <button type='submit' name ='delete' class='btn btn-danger'>Delete</button>
+                                            <a href='movie-detail.php?movie_id={$movieitem['movie_id']}' class='btn btn-danger'>Detail</a>
+                                        </form>
                                 </td>
                             </tr>
                         ";
@@ -156,6 +185,42 @@
             </table>
         </div>
     </div>
+
+        <!-- Modal for Create Movie -->
+        <div class="modal fade" id="createMovieModal" tabindex="-1" aria-labelledby="createMovieModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="createMovieModalLabel">Create Movie</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="createMovieForm">
+                        <div class="mb-3">
+                            <label for="movieTitle" class="form-label">Enter Movie Title</label>
+                            <input type="text" class="form-control" id="movieTitle" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="movieURL" class="form-label">Enter Movie URL</label>
+                            <input type="url" class="form-control" id="movieURL" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="moviePoster" class="form-label">Enter Poster URL</label>
+                            <input type="url" class="form-control" id="moviePoster" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="movieDescription" class="form-label">Enter Description</label>
+                            <textarea class="form-control" id="movieDescription" rows="3" required></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="createdAt" class="form-label">Created At</label>
+                            <input type="date" class="form-control" id="createdAt" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Create</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
-
